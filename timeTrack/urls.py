c012acf036d8de787fe00962f57cb1bbbee47860
life-f <58433392+life-f@users.noreply.github.com>
2021-10-11 14:04:39 +0300

    игшдв мгу

--- timeTrack/urls.py
+++ timeTrack/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls import static
 
@@ -24,4 +24,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*',  include("server.urls"))]
