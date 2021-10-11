1c72b6c5e4777432e3b51ccf5dcf0bfce4ec38c5
life-f <58433392+life-f@users.noreply.github.com>
2021-10-11 15:38:48 +0300

    try fixes bugs

--- timeTrack/settings.py
+++ timeTrack/settings.py
@@ -192,8 +192,8 @@ DJOSER = {
 
 django_heroku.settings(locals())
 
-try:
-    from .local_settings import *
-except ImportError:
-    from .prod_settings import *
-# from .prod_settings import *
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     from .prod_settings import *
+from .prod_settings import *
