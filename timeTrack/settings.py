33ef8cc2f5de1a029e347a18f7781eae6fb34aef
life-f <58433392+life-f@users.noreply.github.com>
2022-04-08 09:39:43 +0300

    email heroku deploy

--- timeTrack/settings.py
+++ timeTrack/settings.py
@@ -193,3 +193,17 @@ django_heroku.settings(locals())
 #     from .prod_settings import *
 from .prod_settings import *
 
+
+# smtp
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'polya.bagrowa@gmail.com'
+# EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = 'vwrdnguwqmavimoi'
+# EMAIL_HOST_PASSWORD= ''
+EMAIL_PORT = 587
+# EMAIL_PORT = 1025
+DEFAULT_FROM_EMAIL = 'polya.bagrowa@gmail.com'
